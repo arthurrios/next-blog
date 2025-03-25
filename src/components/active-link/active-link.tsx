392df6b00986e585749229cea68dd9ba5536c0de
arthur <arthur.rios007@gmail.com>
2025-03-24 21:20:53 -0300

    feat: add eslint and layout

--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -1,17 +1,27 @@
-import { cn } from "@/lib/utils";
-import Link, { LinkProps } from "next/link";
-import { useRouter } from "next/router";
+import { cn } from '@/lib/utils'
+import Link, { LinkProps } from 'next/link'
+import { useRouter } from 'next/router'
 
 type ActiveLinkProps = {
-  children: React.ReactNode;
+  children: React.ReactNode
 } & LinkProps
 
 export function ActiveLink({ children, href, ...props }: ActiveLinkProps) {
   const router = useRouter()
-  const isCurrentPath = router.asPath === href || router.asPath === props.as || router.asPath.startsWith(String(props.as))
+  const isCurrentPath =
+    router.asPath === href ||
+    router.asPath === props.as ||
+    router.asPath.startsWith(String(props.as))
 
   return (
-    <Link href={href} className={cn('text-sm font-medium transition-colors hover:text-blue-500', isCurrentPath ? 'text-blue-500' : 'text-muted-foreground')}>{children}</Link>
-
+    <Link
+      href={href}
+      className={cn(
+        'text-sm font-medium transition-colors hover:text-blue-500',
+        isCurrentPath ? 'text-blue-500' : 'text-muted-foreground',
+      )}
+    >
+      {children}
+    </Link>
   )
 }
