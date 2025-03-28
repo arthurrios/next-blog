16ffd188961962a07bd94c82206de4cd28148e7d
arthur <arthur.rios007@gmail.com>
2025-03-28 16:38:45 -0300

    feat: add post card component

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,5 +1,6 @@
 import { Search } from '@/components/search'
 import { useRouter } from 'next/router'
+import { PostCard } from './components/post-card'
 
 export function BlogList() {
   const router = useRouter()
@@ -9,8 +10,8 @@ export function BlogList() {
     : 'Tips and strategies to boost your business'
 
   return (
-    <div className="flex h-full flex-col pt-25 md:px-0 md:pt-44">
-      <div className="main-container">
+    <div className="flex h-full flex-col pt-25 pb-20 md:px-0 md:pt-44 md:pb-34">
+      <div className="main-container flex w-full flex-col gap-6 md:gap-14">
         <div className="flex w-full flex-col items-start md:flex-row md:justify-between">
           <header className="flex w-full flex-col gap-3">
             <span className="body-tag w-fit rounded-sm bg-cyan-300 px-3 py-1.5 text-center text-cyan-100 uppercase md:text-left">
@@ -24,6 +25,14 @@ export function BlogList() {
             <Search />
           </div>
         </div>
+        <PostCard
+          title="Transformando seu negócio em uma loja virtual"
+          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+          slug="/transforming-your-business-into-an-online-store"
+          image="/assets/first-post.png"
+          date="12/20/2024"
+          author={{ avatar: '/customer-01.png', name: 'Aspen Dokidis' }}
+        />
       </div>
     </div>
   )
---
+++ src/templates/blog/components/post-card/post-card.tsx
@@ -0,0 +1,69 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+type Author = {
+  name: string
+  avatar: string
+}
+
+type PostCardProps = {
+  slug: string
+  title: string
+  description: string
+  image: string
+  date: string
+  author: Author
+}
+
+export function PostCard({
+  slug,
+  title,
+  description,
+  date,
+  image,
+  author,
+}: PostCardProps) {
+  return (
+    <Link
+      href={`/blog/${slug}`}
+      className="w-full max-w-2xl overflow-hidden rounded-xl border border-gray-400 bg-gray-600 transition-all duration-300 hover:border hover:border-blue-300"
+    >
+      {/* Post Content */}
+      <div className="relative overflow-hidden p-2">
+        {/* Date Container */}
+        <div className="absolute top-0 right-0 rounded-bl-[10px] bg-gray-600 pt-2.5 pr-[14px] pb-1.5 pl-2.5 backdrop-blur-sm">
+          <span className="body-xs text-gray-300">{date}</span>
+        </div>
+        <Image
+          src={image}
+          alt=""
+          width={288}
+          height={144}
+          className="h-36 w-full rounded-md object-cover object-center"
+        />
+
+        {/* Post Info */}
+        <div className="px-2 pt-4">
+          <h2 className="heading-xs mb-2 line-clamp-3 text-gray-100">
+            {title}
+          </h2>
+
+          <p className="body-xs text-gray-300">{description}</p>
+
+          <div className="mt-3 flex items-center gap-2 border-t border-gray-400 pt-3 pb-2">
+            <div className="relative size-5 overflow-hidden rounded-full border border-blue-200">
+              <Image
+                src={author?.avatar}
+                alt=""
+                fill
+                className="rounded-md object-cover"
+              />
+            </div>
+
+            <span className="body-sm text-gray-300">{author?.name}</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
