5caffa50c53f539409a4d09221da4ff1bff9cd92
arthur <arthur.rios007@gmail.com>
2025-04-09 09:56:19 -0300

    chore: add seo in blog list page

--- next-blog-app-router/src/app/blog/page.tsx
+++ next-blog-app-router/src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { BlogList } from '@/templates/blog'
 import { allPosts } from 'contentlayer/generated'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Tips and strategies to boost your business',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'Tips and strategies to boost your business',
+    url: 'https://next-blog-blue-one.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'en-US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://next-blog-blue-one.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+}
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
