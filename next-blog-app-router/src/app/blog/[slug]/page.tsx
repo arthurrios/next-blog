e9096d755ffa65aa3fc178e3bd2c4f31ad583fa4
arthur <arthur.rios007@gmail.com>
2025-04-09 10:03:27 -0300

    chore: add seo in blog post page

--- next-blog-app-router/src/app/blog/[slug]/page.tsx
+++ next-blog-app-router/src/app/blog/[slug]/page.tsx
@@ -1,11 +1,45 @@
 import PostPage from '@/pages/blog-page/[slug]'
 import { allPosts } from 'contentlayer/generated'
+import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 type BlogPostPageProps = {
   params: Promise<{
     slug: string
   }>
+}
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params
+  const post = allPosts.find((post) => post.slug === slug)
+
+  if (!post) {
+    return {}
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      url: `https://next-blog-blue-one.vercel.app/blog/${post.slug}`,
+      siteName: 'Site.Set',
+      locale: 'en-US',
+      type: 'website',
+      images: [
+        {
+          url: post.image,
+          width: 800,
+          height: 600,
+          alt: post.title,
+        },
+      ],
+    },
+  }
 }
 
 export async function generateStaticParams() {
--- next-blog-app-router/src/app/layout.tsx
+++ next-blog-app-router/src/app/layout.tsx
@@ -5,6 +5,23 @@ import { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Site.Set',
   description: 'Sell your products as an affiliate in one place only',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Sell your products as an affiliate in one place only',
+    url: 'https://next-blog-blue-one.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'en-US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://next-blog-blue-one.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
