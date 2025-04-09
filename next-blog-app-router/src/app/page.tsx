3654290c8f9e2e41ad7b89141f0698cc035820e6
arthur <arthur.rios007@gmail.com>
2025-04-09 09:45:16 -0300

    feat: add og image

--- next-blog-app-router/src/app/page.tsx
+++ next-blog-app-router/src/app/page.tsx
@@ -1,4 +1,16 @@
 import { LandingPage } from '@/templates/landing-page'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Site.Set',
+  description: 'Sell your products as an affiliate in one place only',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Sell your products as an affiliate in one place only',
+    url: 'https://next-blog-blue-one.vercel.app/og-image.jpg',
+  },
+}
 
 export default function Home() {
   return <LandingPage />
