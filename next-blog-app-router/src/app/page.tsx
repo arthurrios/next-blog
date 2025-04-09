1c0c398a783e38c6e994d345cd4d4353800db0a4
arthur <arthur.rios007@gmail.com>
2025-04-09 09:51:48 -0300

    feat: add seo in landing page

--- next-blog-app-router/src/app/page.tsx
+++ next-blog-app-router/src/app/page.tsx
@@ -9,6 +9,17 @@ export const metadata: Metadata = {
     title: 'Site.Set',
     description: 'Sell your products as an affiliate in one place only',
     url: 'https://next-blog-blue-one.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'en-US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://next-blog-blue-one.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
   },
 }
 
